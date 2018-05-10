9135fc4e761f3eb71cc339c36d55052c6e1bafc2
Martin Rodriguez <mtr@pdx.edu>
2018-05-10 14:44:32 -0700

    third commit

--- freq2midi/freq2midi.c
+++ freq2midi/freq2midi.c
@@ -1,10 +1,17 @@
-/* Calculate frequency of a MIDI note number. Taken from The Audio Programming Book */
+/* Find nearest MIDI note to a given frequency in Hz. 
+ * Uses the log rule: 
+ *  log_a(N) = log_b(N) / log_b(a)
+ *      to find the log of a value to base 'semitone_ratio'.
+ *
+ * Taken from The Audio Programming Book */
+
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
     double semitone_ratio;
+    double fracmidi;
     double c0; /* for frequency of MIDI Note 0 */
     double c5; /* for frequency of Middle C */
     double frequency; /* ... which we want to find, */
@@ -18,11 +25,11 @@ int main()
     /* MIDI Note 0 is C, 5 octaves below Middle C */
     c0 = c5 * pow(0.5, 5);
 
-    /* calculate a frequency for a given MIDI Note Number */
-    midinote = 73; /* C# above A 440 */
-    frequency = c0 * pow(semitone_ratio, midinote);
-
-    printf("MIDI Note %d has frequency %f\n", midinote, frequency);
+    frequency = 400.0;
+    fracmidi = log(frequency / c0) / log(semitone_ratio);
+    /* Round fracmidi to the nearest whole number */
+    midinote = (int) (fracmidi + 0.5);
+    printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote);
 
     return 0;
 }
