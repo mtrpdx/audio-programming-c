98ee70587231f1a5ba89566a641f12a2a0a048e7
Martin Rodriguez <mtr@pdx.edu>
2018-05-10 14:55:32 -0700

    fourth commit

---
+++ print_sizeof/print_sizeof.c
@@ -0,0 +1,13 @@
+/* Prints result of sizeof() on data types */
+#include <stdio.h>
+
+int main()
+{
+    printf("The size of an int is %d\n", sizeof(int));
+    printf("The size of a char is %d\n", sizeof(char));
+    printf("The size of a short is %d\n", sizeof(short));
+    printf("The size of a long is %d\n", sizeof(long));
+    printf("The size of a float is %d\n", sizeof(float));
+    printf("The size of a double is %d\n", sizeof(double));
+    return 0;
+}
