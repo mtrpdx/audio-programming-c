927fca68880de7faa5b0badb9331a4dc5e72c2cd
Martin Rodriguez <mtr@pdx.edu>
2018-05-15 15:09:13 -0700

    Add pitchbend calculation to freq2midi, improve comments

--- freq2midi/freq2midi.c
+++ freq2midi/freq2midi.c
@@ -14,8 +14,15 @@ int main()
     double fracmidi;
     double c0; /* for frequency of MIDI Note 0 */
     double c5; /* for frequency of Middle C */
-    double frequency; /* ... which we want to find, */
-    int    midinote;     /* ... given this note. */
+    double frequency; /* ... given this frequency */
+    double low_frequency;   /* we calculate the closest midi note freq on lower side */
+    double high_frequency;  /* ... and the closest midi note freq on higher side */
+    double difference; 
+    int    midinote;     /* ... we want to find this note. */
+    int    bend;
+    int    low_side;    /* closest MIDI note on low side */
+    int    high_side;   /* closest MIDI note on high side*/
+    char*   sign;
 
     /* calculate required numbers */
 
@@ -25,11 +32,31 @@ int main()
     /* MIDI Note 0 is C, 5 octaves below Middle C */
     c0 = c5 * pow(0.5, 5);
 
-    frequency = 400.0;
+    frequency = 430.0;
     fracmidi = log(frequency / c0) / log(semitone_ratio);
+    low_side = (int) floor(fracmidi);
+    high_side = (int) ceil(fracmidi);
+
     /* Round fracmidi to the nearest whole number */
     midinote = (int) (fracmidi + 0.5);
+    
+    low_frequency = c0 * pow(semitone_ratio,low_side);
+    high_frequency = c0 * pow(semitone_ratio,high_side);
+
     printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote);
+    if (low_side == midinote) 
+    {
+        difference = frequency - low_frequency;
+        sign = "+";
+    } 
+    else 
+    {
+        difference = high_frequency - frequency;
+        sign = "-";
+    }
+    difference = difference / (high_frequency - low_frequency);
+    bend = (int) (difference * 100 + 0.5);
+    printf("pitchbend = %s%d%%\n",sign, bend);
 
     return 0;
 }
--- midi2freq/midi2freq.c
+++ midi2freq/midi2freq.c
@@ -8,21 +8,29 @@ int main()
     double c0; /* for frequency of MIDI Note 0 */
     double c5; /* for frequency of Middle C */
     double frequency; /* ... which we want to find, */
+    double string_length;
     int    midinote;     /* ... given this note. */
+    int    i;
 
     /* calculate required numbers */
 
-    semitone_ratio = pow(2, 1/12.0); /* approx. 1.0594631 */
+    semitone_ratio = pow(2.0, 1.0/12.0); /* approx. 1.0594631 */
     /* find Middle C, three semitones above low A = 220 */
-    c5 = 220.0 * pow(semitone_ratio, 3);
+    c5 = 220.0 * pow(semitone_ratio, 3.0);
     /* MIDI Note 0 is C, 5 octaves below Middle C */
-    c0 = c5 * pow(0.5, 5);
+    c0 = c5 * pow(0.5, 5.0);
 
     /* calculate a frequency for a given MIDI Note Number */
     midinote = 73; /* C# above A 440 */
-    frequency = c0 * pow(semitone_ratio, midinote);
-
+    frequency = c0 * pow(semitone_ratio, (double) midinote);
+    
+    string_length = 660.0;
     printf("MIDI Note %d has frequency %f\n", midinote, frequency);
-
+    printf("Sounding length of string sounding C4 is %f cm\n", string_length);
+    for (i=1;i<13;i=i+1){
+       string_length = string_length / pow(semitone_ratio, 1.0);
+       printf("Sounding length of string for the next semitone up is %f cm\n", 
+               string_length);
+    }
     return 0;
 }
